ca26c927e2fe48bdb44d0f4584d390fcd91f5461
RonieGSS <ronievincent.gss@gmail.com>
2018-09-20 17:11:01 +0800

    Modified product model

--- src/laravel/app/Product.php
+++ src/laravel/app/Product.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Traits\HasRoles;
 
 class Product extends Model
 {
+	use HasRoles;
     /**
      * The attributes that are mass assignable.
      *	
